97487463cfa465b5f71c9f0bc5da61dcbaf775ad
Konik Rahman <konikrahman06@gmail.com>
2024-10-24 05:04:59 +0300

    24/10/2024, index.js: writing the js code there, variable, console etc.

---
+++ A smiple calculator/index.js
@@ -0,0 +1,21 @@
+
+
+// create a variable for storing the user value.
+
+let number1 = parseFloat(prompt("Enter your first value: "));
+let number2 = parseFloat(prompt("Enter your second value: "));
+
+// we can create new variable for storing the result.
+// I am not going to do this. I will print the result with description.
+
+// addition
+document.write(number1 + " + "+ number2 + " = "+ (number1 + number2) + "<br>");
+
+// subtraction
+document.write(number1 + " - "+ number2 + " = "+ (number1 - number2) + "<br>");
+
+// multiplication
+document.write(number1 + " * "+ number2 + " = "+ (number1 * number2) + "<br>");
+
+// divide
+document.write(number1 + " / "+ number2 + " = "+ (number1 / number2) + "<br>");
