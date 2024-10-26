9aac829ffa4db8d07ee74c9ef9f46af9c13a057a
Konik Rahman <konikrahman06@gmail.com>
2024-10-26 23:48:26 +0300

    26/10/2024, index.js:  create js code for checking the letter , is it vowel or consonant

---
+++ vowel-checking-by-using-switch/index.js
@@ -0,0 +1,31 @@
+
+
+const letter = prompt("Enter a alpabet: ");
+
+letter.toLowerCase();
+
+switch(letter){
+    case "a":
+        console.log("it is vowel.");
+        document.write("it is vowel.");
+        break;
+    case "e":
+        console.log("it is vowel");
+        document.write("it is vowel.");
+        break;
+    case "i":
+        console.log("it is vowel.");
+        document.write("it is vowel.");
+        break;
+    case "o":
+        console.log("it is vowel");
+        document.write("it is vowel.");
+        break;
+    case "u":
+        console.log("it is vowel.");
+        document.write("it is vowel.");
+        break;
+    default:
+        console.log("it is consonant");
+        document.write("it is consonant.");
+}
