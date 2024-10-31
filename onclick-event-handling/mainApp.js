75f91223830960478be07904a5d21d93ffb36184
Konik Rahman <konikrahman06@gmail.com>
2024-10-31 07:13:12 +0200

    31/10/2024, mainApp.js: two funtion handling the source image and passing it in html file

---
+++ onclick-event-handling/mainApp.js
@@ -0,0 +1,12 @@
+
+
+
+let image = document.querySelector('#myImage');
+
+function myNature(){
+    image.src = 'images/nature.jpg';
+}
+
+function mySunrise(){
+    image.src = 'images/sunrise.jpg';
+}
